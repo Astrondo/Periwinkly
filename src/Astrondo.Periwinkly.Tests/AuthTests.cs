c38a5228eb9616ce9bd791d010d09c8cafbf3e7a
Stephen Roberts <ste@Astrondo.com>
2022-04-10 22:10:33 +0100

    Fixing unit test

--- src/Astrondo.Periwinkly.Tests/AuthTests.cs
+++ src/Astrondo.Periwinkly.Tests/AuthTests.cs
@@ -11,7 +11,7 @@ namespace Astrondo.Periwinkly.Tests
         {
             var result = ShellyHttpClient.CreateAuthModel("password", "shellypro1-30c6f7849da4", 1649523350, 1649523352450, "admin", "SHA-256");
 
-            Assert.AreEqual("1aa5ff74be67278159e12165546acf59fc6b8bb8a6f0bf42f0f8497519c12e0e", result);
+            Assert.AreEqual("1aa5ff74be67278159e12165546acf59fc6b8bb8a6f0bf42f0f8497519c12e0e", result.Response);
         }
     }
 }
